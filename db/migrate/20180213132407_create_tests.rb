cbea765a522f2c51a569bd813516ba84a119ad49
AlexAlexis <np.zaporoje.sb2@gmail.com>
2018-02-13 15:38:32 +0200

    db migrated

---
+++ db/migrate/20180213132407_create_tests.rb
@@ -0,0 +1,13 @@
+class CreateTests < ActiveRecord::Migration[5.1]
+  def up
+    create_table :tests do |t|
+      t.string :names
+      t.text :comments
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :test
+  end
+end
